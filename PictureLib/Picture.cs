8cbfd0261f8cd3d1fcb5b71154fc0c75e7a76861
Thomas Strauß <thomas.strauss@nexgo.de>
2026-04-29 22:27:38 +0200

    Add picture library

---
+++ PictureLib/Picture.cs
@@ -0,0 +1,7 @@
+﻿namespace PictureLib
+{
+    public sealed class Picture
+    {
+
+    }
+}
---
+++ PictureLibTests/PictureTests.cs
@@ -0,0 +1,20 @@
+﻿
+using PictureLib;
+
+namespace PictureLibTests
+{
+    internal sealed class PictureTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void PictureTest()
+        {
+            Picture picture = new Picture();
+            Assert.IsNotNull(picture);
+        }
+    }
+}
